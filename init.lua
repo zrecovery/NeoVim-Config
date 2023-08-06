ffc06046fbfde55b5c0444df25d6325628ebfbda
zrecovery <zrecovery@outlook.com>
2023-08-06 21:55:39 +0800

    feat: 添加Lint功能，并拆分模块

--- init.lua
+++ init.lua
@@ -2,8 +2,9 @@ local keymap = require('keymap')
 local nmap, imap, cmap, xmap = keymap.nmap, keymap.imap, keymap.cmap, keymap.xmap
 local silent, noremap = keymap.silent, keymap.noremap
 local opts = keymap.new_opts
-local cmd = keymap.cmd
 
+local cmd = keymap.cmd
+local conf = require('config')
 --基础配置
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
@@ -14,114 +15,109 @@ vim.opt.expandtab = true
 vim.opt.number = true
 vim.opt.relativenumber = true
 
-
 -- 快捷键
 vim.g.mapleader = ' '
+
 nmap({ ' ', '', opts(noremap) })
 xmap({ ' ', '', opts(noremap) })
--- LSP快捷键
--- 格式化
-nmap({ '<Leader>lf', function()
-    vim.lsp.buf.format { async = true }
-end, opts(noremap, silent) })
--- Neotree配置
-nmap({ '<Leader>e', cmd('Neotree toggle'), opts(noremap, silent) })
-
-local conf = {}
-
--- 补全配置
-function conf.lspcmp()
-    -- Add additional capabilities supported by nvim-cmp
-    local capabilities = require('cmp_nvim_lsp').default_capabilities()
-
-    local lspconfig = require('lspconfig')
-
-    -- Enable some language servers with the additional completion capabilities offered by nvim-cmp
-    local servers = { 'lua_ls', 'pyright', 'tsserver', 'cssls' }
-    for _, lsp in ipairs(servers) do
-        lspconfig[lsp].setup({
-            capabilities = capabilities,
-        })
-    end
-
-    -- luasnip setup
-    local luasnip = require('luasnip')
-
-    -- nvim-cmp setup
-    local cmp = require('cmp')
-    cmp.setup({
-        snippet = {
-            expand = function(args)
-                luasnip.lsp_expand(args.body)
-            end,
-        },
-        mapping = cmp.mapping.preset.insert({
-            ['<C-Space>'] = cmp.mapping.complete(),
-            ['<CR>'] = cmp.mapping.confirm({
-                behavior = cmp.ConfirmBehavior.Replace,
-                select = true,
-            }),
-            ['<Tab>'] = cmp.mapping(function(fallback)
-                if cmp.visible() then
-                    cmp.select_next_item()
-                elseif luasnip.expand_or_jumpable() then
-                    luasnip.expand_or_jump()
-                else
-                    fallback()
-                end
-            end, { 'i', 's' }),
-            ['<S-Tab>'] = cmp.mapping(function(fallback)
-                if cmp.visible() then
-                    cmp.select_prev_item()
-                elseif luasnip.jumpable(-1) then
-                    luasnip.jump(-1)
-                else
-                    fallback()
-                end
-            end, { 'i', 's' }),
-        }),
-        sources = {
-            { name = 'nvim_lsp' },
-            { name = 'luasnip' },
-        },
-    })
-end
-
-conf.lspcmp()
-
-conf.treesitter = {
-    ensure_installed = { "python", "typescript", "javascript", "tsx", "jsx", "css", "json", "lua" },
-    highlight = {
-        enable = true
-    },
-    indent = {
-        enable = true
-    }
-}
 
 -- 包管理
-return require('packer').startup(function(use)
-    -- 管理包
-    use('wbthomason/packer.nvim')
-    use({ 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate', config = conf.treesitter })
-    -- LSP及补全
-    use({ 'neovim/nvim-lspconfig' })
-    use({
-        'hrsh7th/nvim-cmp',
-        requires = {
-            'hrsh7th/cmp-nvim-lsp',     -- LSP source for nvim-cmp
-            'saadparwaiz1/cmp_luasnip', -- Snippets source for nvim-cmp
-            'L3MON4D3/LuaSnip',
-        },                              -- Snippets plugin
-    })
-    -- Neotree 显示文件树
-    use({
-        'nvim-neo-tree/neo-tree.nvim',
-        branch = 'v3.x',
-        requires = {
-            'nvim-lua/plenary.nvim',
-            'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
-            'MunifTanjim/nui.nvim',
-        },
-    })
+local packer = require('packer')
+
+packer.startup(function(use)
+  -- 管理包
+  use('wbthomason/packer.nvim')
+  --高亮显示
+  use({ 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate', config = conf.treesitter })
+  -- LSP及补全
+  use({ 'neovim/nvim-lspconfig' })
+  use({
+    'hrsh7th/nvim-cmp',
+    requires = {
+      'hrsh7th/cmp-nvim-lsp', -- LSP source for nvim-cmp
+      'saadparwaiz1/cmp_luasnip', -- Snippets source for nvim-cmp
+      'L3MON4D3/LuaSnip',
+    },
+    config = conf.lspcmp,
+  })
+  -- Neotree 显示文件树
+  use({
+    'nvim-neo-tree/neo-tree.nvim',
+    branch = 'v3.x',
+    requires = {
+      'nvim-lua/plenary.nvim',
+      'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
+      'MunifTanjim/nui.nvim',
+    },
+  })
+  use({ 'mfussenegger/nvim-lint', config = conf.lint })
 end)
+
+-- LSP快捷键
+-- 对付过长提示
+nmap({
+  '<Leader>ll',
+  function()
+    vim.diagnostic.open_float(0, { scope = 'line' })
+  end,
+  opts(noremap, silent),
+})
+
+-- 代码行为
+nmap({
+  '<Leader>la',
+  function()
+    vim.lsp.buf.code_action({ async = true })
+  end,
+  opts(noremap, silent),
+})
+-- 格式化
+nmap({
+  '<Leader>lf',
+  function()
+    vim.lsp.buf.format({ async = true })
+  end,
+  opts(noremap, silent),
+})
+-- 查看定义
+nmap({
+  '<Leader>ld',
+  function()
+    vim.lsp.buf.definition({ async = true })
+  end,
+  opts(noremap, silent),
+})
+-- 显示提示
+nmap({
+  '<Leader>lh',
+  function()
+    vim.lsp.buf.hover({ async = true })
+  end,
+  opts(noremap, silent),
+})
+-- 查看实现
+nmap({
+  '<Leader>li',
+  function()
+    vim.lsp.buf.implementation({ async = true })
+  end,
+  opts(noremap, silent),
+})
+-- 查看引用
+nmap({
+  '<Leader>lr',
+  function()
+    vim.lsp.buf.references({ async = true })
+  end,
+  opts(noremap, silent),
+})
+-- 重命名
+nmap({
+  '<Leader>rn',
+  function()
+    vim.lsp.buf.rename()
+  end,
+})
+
+-- Neotree配置
+nmap({ '<Leader>e', cmd('Neotree toggle'), opts(noremap, silent) })
---
+++ lua/config.lua
@@ -0,0 +1,113 @@
+conf = {}
+-- 补全配置
+function conf.lspcmp()
+  -- Add additional capabilities supported by nvim-cmp
+  local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
+  local lspconfig = require('lspconfig')
+
+  -- Enable some language servers with the additional completion capabilities offered by nvim-cmp
+  local servers = { 'pyright', 'tsserver', 'cssls' }
+  for _, lsp in ipairs(servers) do
+    lspconfig[lsp].setup({
+      capabilities = capabilities,
+    })
+  end
+
+  lspconfig.lua_ls.setup({
+    settings = {
+      Lua = {
+        runtime = {
+          -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+          version = 'LuaJIT',
+        },
+        diagnostics = {
+          -- Get the language server to recognize the `vim` global
+          globals = { 'vim' },
+        },
+        workspace = {
+          -- Make the server aware of Neovim runtime files
+          library = vim.api.nvim_get_runtime_file('', true),
+        },
+        -- Do not send telemetry data containing a randomized but unique identifier
+        telemetry = {
+          enable = false,
+        },
+      },
+    },
+  })
+
+  -- luasnip setup
+  local luasnip = require('luasnip')
+
+  -- nvim-cmp setup
+  local cmp = require('cmp')
+  cmp.setup({
+    snippet = {
+      expand = function(args)
+        luasnip.lsp_expand(args.body)
+      end,
+    },
+    mapping = cmp.mapping.preset.insert({
+      ['<C-Space>'] = cmp.mapping.complete(),
+      ['<CR>'] = cmp.mapping.confirm({
+        behavior = cmp.ConfirmBehavior.Replace,
+        select = true,
+      }),
+      ['<Tab>'] = cmp.mapping(function(fallback)
+        if cmp.visible() then
+          cmp.select_next_item()
+        elseif luasnip.expand_or_jumpable() then
+          luasnip.expand_or_jump()
+        else
+          fallback()
+        end
+      end, { 'i', 's' }),
+      ['<S-Tab>'] = cmp.mapping(function(fallback)
+        if cmp.visible() then
+          cmp.select_prev_item()
+        elseif luasnip.jumpable(-1) then
+          luasnip.jump(-1)
+        else
+          fallback()
+        end
+      end, { 'i', 's' }),
+    }),
+    sources = {
+      { name = 'nvim_lsp' },
+      { name = 'luasnip' },
+    },
+  })
+end
+
+function conf.treesitter()
+  require('nvim-treesitter.configs').setup({
+    ensure_installed = { 'python', 'typescript', 'javascript', 'tsx', 'jsx', 'css', 'json', 'lua' },
+    sync_install = false,
+    auto_install = true,
+    ignore_install = {},
+    highlight = {
+      enable = true,
+    },
+    indent = {
+      enable = true,
+    },
+    run = ':TSUpdate',
+  })
+end
+
+-- Lint初始化
+function conf.lint()
+  local lint = require('lint')
+  lint.linters_by_ft = {
+    typescript = { 'eslint' },
+    javascript = { 'eslint' },
+  }
+  vim.api.nvim_create_autocmd({ 'BufWritePost' }, {
+    callback = function()
+      lint.try_lint()
+    end,
+  })
+end
+
+return conf
